29b617bd840c2ecdc8de1f7f4571289a88cae626
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-25 15:29:53 -0500

    V6

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ def main():
         if len(sys.argv) < 2:
             raise IndexError("Insufficient arguments provided!")
         file_path = sys.argv[1]
+        if len(sys.argv) >= 3:
+            command = sys.argv[2]
+            if command == 'view':
+                tasks = read_todo_file(file_path)
+                print("Tasks:")
+                for task in tasks:
+                    print(task)
+            else:
+                raise ValueError("Command not found!")
         print(f"Arguments received: {sys.argv[1:]}")
         tasks = read_todo_file(file_path)
         print(f"Tasks in list: {tasks}")
-    except IndexError as x:
-        print(f"Error: {x}")
+    except (IndexError, ValueError) as x:
+        print(f"{x}")
     except Exception as x:
         print(f"An unexpected error ocurred: {x}")
 if __name__ == "__main__":
--- todo_manager.py
+++ todo_manager.py
@@ -7,13 +7,12 @@ def read_todo_file(file_path):
         with open(file_path, 'r') as file:
             return file.read().splitlines()
     except FileNotFoundError:
-        print(f"File {file_path} not found! Returning to an empty to do list.")
+        print(f"File {file_path} not found! Returning to an empty to-do list.")
         return []
-
-    raise NotImplementedError
-
 
 def write_todo_file(file_path, tasks):
     """Writes tasks to a file, one per line."""
-    # TODO: Implementar escritura de tareas según README.md
-    raise NotImplementedError
+    with open(file_path, 'w') as file:
+        for task in tasks:
+            file.write(f"{task}\n")
+
