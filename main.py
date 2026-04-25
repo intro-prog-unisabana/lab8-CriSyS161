9058f4f5b7dd8f56bacb65f941be1cbb1cdd66db
Cristian Andrés Sánchez Suárez <cristiansasu@unisabana.edu.co>
2026-04-25 16:04:29 -0500

    V7

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 from todo_manager import read_todo_file, write_todo_file
 def main():
     try:
+        if "--help" in sys.argv:
+            print("Usage: python main.py <file_path> <command> [arguments]...")
+            return
         if len(sys.argv) < 2:
             raise IndexError("Insufficient arguments provided!")
         file_path = sys.argv[1]
@@ -13,11 +16,27 @@ def main():
                 print("Tasks:")
                 for task in tasks:
                     print(task)
+            elif command == "add":
+                if len(sys.argv) < 4:
+                    raise IndexError('Task description required for "add"')
+                new_tasks = sys.argv[3]
+                tasks = read_todo_file(file_path)
+                tasks.append(new_tasks)
+                write_todo_file(file_path, tasks)
+                print(f'Task "{new_tasks}" added.')
+            elif command == "remove":
+                if len(sys.argv) < 4:
+                    raise IndexError('Task description required for "remove"')
+                tasks_remove = sys.argv[3]
+                tasks = read_todo_file(file_path)
+                if tasks_remove in tasks:
+                    tasks.remove(tasks_remove)
+                    write_todo_file(file_path, tasks)
+                    print(f'Task "{tasks_remove}" removed.')
+                else:
+                    print(f'Task"{tasks_remove}" not found')
             else:
                 raise ValueError("Command not found!")
-        print(f"Arguments received: {sys.argv[1:]}")
-        tasks = read_todo_file(file_path)
-        print(f"Tasks in list: {tasks}")
     except (IndexError, ValueError) as x:
         print(f"{x}")
     except Exception as x:
